bdf48f69f57b2305a21234fedac0ce18ad7ed38c
nhim-21730055 <165756103+nhim-uit@users.noreply.github.com>
2025-07-14 06:36:24 +0700

    added instructor's solution

--- identify_a_palindrome.py
+++ identify_a_palindrome.py
@@ -2,6 +2,9 @@
 # 14 Jul, 2025
 # Identify a palindrome
 
+import re
+
+
 # my solution
 def alpha(s):
     return ''.join([i for i in s if i.isalpha()])
@@ -11,9 +14,16 @@ def solve(s):
     return alpha(s[::-1]).lower() == alpha(s).lower()
 
 
+# instructor's solution
+def solve2(s):
+    forwards = ''.join(re.findall(r'[a-z]+', s.lower()))
+    backwards = forwards[::-1]
+    return forwards == backwards
+
+
 print(solve('abba'))
 print(solve('abcba'))
 print(solve('d'))
-print(solve('da'))
-print(solve('race car')) # true
-print(solve('Go hang a salami - I\'m a lasagna hog.')) # true
+print(solve2('da'))
+print(solve2('race car')) # true
+print(solve2('Go hang a salami - I\'m a lasagna hog.')) # true
