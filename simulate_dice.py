0b0fc189466b9b25b1651fbe93fa8cd1c645e20d
nhim-21730055 <165756103+nhim-uit@users.noreply.github.com>
2025-07-22 06:58:32 +0700

    added histogram

--- simulate_dice.py
+++ simulate_dice.py
@@ -2,6 +2,7 @@
 # 22 Jul, 2025
 # simulate a dice
 import random
+import matplotlib.pyplot as plt
 
 NO_OF_TRIALS = 1_000_000
 res = {}
@@ -13,13 +14,27 @@ def roll_dice(dice, no_of_trials=NO_OF_TRIALS):
         for j in range(dice):
             face = random.randint(1, 6)
             s += face
-        res[s] = res.get(s, 0) + 1
+        res[s] = (res.get(s, 0) + 1)
 
-    for i in res:
-        print(f'Face {i} = {res[i]/NO_OF_TRIALS*100:.2f}%')
+    # for i in res:
+    #     print(f'Face {i} = {res[i]/NO_OF_TRIALS*100:.2f}%')
 
     return res
 
 
-roll_dice(4)
+def draw(data):
+    x = list(data.keys())
+    y = list(data.values())
 
+    # Plot the graph
+    plt.figure(figsize=(8, 5))
+    plt.bar(x, y, color='b', width=0.8)
+    plt.title("Graph from Dictionary")
+    plt.xlabel("Face")
+    plt.ylabel("Percentage (%)")
+    plt.grid(True)
+    plt.show()
+
+
+if __name__=='__main__':
+    draw(roll_dice(6))
